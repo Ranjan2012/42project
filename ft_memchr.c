1bbd0a61f76851f1b936f9de9d44b5e8ed32f042
jonting56 <87597182+jonting56@users.noreply.github.com>
2022-02-17 15:32:54 +1030

    Add files via upload
    Edited memchr.c

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,31 +6,12 @@
 /*   By: rmallawa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 09:27:50 by rmallawa          #+#    #+#             */
-/*   Updated: 2022/01/18 09:34:46 by rmallawa         ###   ########.fr       */
+/*   Updated: 2022/02/17 15:31:16 by jting            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-/*
-** LIBRARY: <string.h>
-** SYNOPSIS: locate byte in byte string
-**
-** DESCRIPTION:
-** 		The memchr() function locates the first occurrence of c (converted to an
-**	unsigned char) in string s.
-*/
-#include "libft.h"
 
-void	*ft_memchr(const void *s, int c, size_t n)
+#include <stdio.h>
+int main
 {
-	char	*str;
-	size_t	i;
-
-	i = 0;
-	str = (char *)s;
-	while (i < n)
-	{
-		if ((unsigned char)str[i] == (unsigned char)c)
-			return ((char *)s + i);
-		i++;
-	}
-	return (NULL);
+	prinft("hello world");
 }
